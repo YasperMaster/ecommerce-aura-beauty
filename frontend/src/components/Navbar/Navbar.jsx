1123d150bdab3c4d4d541077462a3f40f5f8ee8a
Tomas Yasparra <tomasyas@gmail.com>
2026-05-15 07:15:32 -0300

    create frontend

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <header>
+            <nav className="">
+                <div className="navbar-start">
+                    <a className="btn btn-ghost text-xl" to="/"> Ecommerce</a>
+                </div>
+                <div className="navbar-end gap-3"></div>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
