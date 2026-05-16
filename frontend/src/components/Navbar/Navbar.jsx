a86b45f00ddcc17bfc9cfba7b89146a463afa3a3
Tomas Yasparra <tomasyas@gmail.com>
2026-05-16 20:02:54 -0300

    finished navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,20 @@
+import Cart from "./Cart"
+import AuthButtons from "./AuthButtons"
+import UserDropDown from "./UserDropDown"
+
 const Navbar = () => {
     return (
         <header>
-            <nav className="">
+            <AuthButtons/>
+            <nav className="navbar bg-base-100 shadow-sm lg:rounded-box w-full">
                 <div className="navbar-start">
                     <a className="btn btn-ghost text-xl" to="/"> Ecommerce</a>
                 </div>
-                <div className="navbar-end gap-3"></div>
+                <div className="navbar-end gap-3">
+                    <a className="btn btn-primary">Dashboard</a>
+                    <Cart />
+                    <UserDropDown/>
+                </div>
             </nav>
         </header>
     )
