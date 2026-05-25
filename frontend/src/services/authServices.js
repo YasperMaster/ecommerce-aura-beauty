c5a43de7463e22965095b523620b1ce7c8ef7e6b
Tomas Yasparra <tomasyas@gmail.com>
2026-05-25 18:52:37 -0300

    create registerService function

--- frontend/src/services/authServices.js
+++ frontend/src/services/authServices.js
@@ -1,3 +1,9 @@
+import axios from "axios"
+
+const API_URL = import.env.VITE_BACKEND + "/auth"
+
+axios.defaults.withCredentials = true
+
 export const getProfileService = async () => {
     
 }
@@ -6,8 +12,19 @@ export const loginService = async () => {
 
 }
 
-export const registerService = async () => {
-    
+export const registerService = async (data, reset, setRedirect, checkSession) => {
+    try {
+        const response = await axios.post(`${API_URL}/register`, data, {
+            headers: { "Content-Type": "application/json"},
+            withCredentials: true,
+        })
+
+        if (response.status === 201 || response.status === 200) {
+            alert("Usuario registrado exitosamente")
+        }
+    } catch (error) {
+        alert("Error al registrar el usuario")
+    }
 }
 
 export const logoutService = async () => {
