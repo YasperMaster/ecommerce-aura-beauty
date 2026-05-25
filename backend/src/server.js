8522469d6f86a0987781564a035dec04e99b2065
Tomas Yasparra <tomasyas@gmail.com>
2026-05-25 18:54:33 -0300

    add cors to server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,8 @@ import express from "express"
 import { connectDB, disconnectDB } from "./config/conifgdb.js"
 import dotenv from "dotenv"
 import authRoutes from "./routes/authRoutes.js"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
 dotenv.config()
 
@@ -9,7 +11,17 @@ const app = express()
 
 const PORT = 3001
 
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "Cookie", "Set-Cookie"],
+    credentials: true,
+}))
+
+app.use(cookieParser())
+
 app.use(express.json())
+
 app.use("/api/auth", authRoutes)
 
 connectDB().then(() => {
